8956b84e5a9e2abb682648136e92fbe62dc4eb00
oiDreas <andreasanthony92@gmail.com>
2024-10-18 16:01:55 -0300

    Subindo exercicios

--- Função/Exercicios-Javascript/Resolução/ex6.js
+++ Função/Exercicios-Javascript/Resolução/ex6.js
@@ -1,9 +1,15 @@
 function calcularJurosSimples(inicial, taxa, tempo) {
-    const capitalInicial = inicial; 
-    const taxaJuros = taxa;
-    const tempoAplicado = tempo;
 
-    let calculoSimples = Number(((taxaJuros * capitalInicial) * tempoAplicado)).toFixed()
-    console.log(`R$ ${calculoSimples.replace(".", ",")}`)
+    const calculo = taxa * inicial * tempo
+    const resultado = Number(calculo + inicial).toFixed(2)
+    console.log(`R$ ${resultado.replace(".", ",")}`)
 }
-calcularJurosSimples(10, 0.1, 12)
+calcularJurosSimples(10, 0.01, 12)
+
+
+function calcularJurosComposto(inicial, taxa, tempo) {
+    const calculo =  1 + taxa
+    const montante = (Math.pow(calculo, tempo) * inicial).toFixed(2)
+    console.log(`R$ ${montante}`)
+}
+calcularJurosComposto(10, 0.01, 12)

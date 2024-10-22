34d9e085d9c05cadeae31b5af62d30f2415de2f7
oiDreas <andreasanthony92@gmail.com>
2024-10-22 16:05:22 -0300

    Subindo exercicios

--- Função/Exercicios-Javascript/Resolução/ex8.js
+++ Função/Exercicios-Javascript/Resolução/ex8.js
@@ -1,14 +1,25 @@
-let pontos = [10, 20, 20, 8, 25, 3, 0, 30, 1]
+let pontos = ('10, 20, 20, 8, 25, 3, 0, 30, 1')
 
 
 function pontosPorTemporada(pontos) {
+    let pontuacao = pontos.split(", ")
+    let maiorPontuacao = Number(pontuacao[0])
+    let menorPontuacao = Number(pontuacao[0])
     let quebraRecorde = 0
-    pontos.forEach(ponto => {
-        if (ponto >= pontos) {
-            maiorPontos.push(ponto);
-            quebraRecorde += 1
-        } 
-    });
+    let piorJogo = 1
+
+    for (let i = 1; i < pontuacao.length; i++) {
+        let ponto = Number(pontuacao[i])
+        if (ponto > maiorPontuacao) {
+            quebraRecorde++
+            maiorPontuacao = ponto
+        } else if (ponto < menorPontuacao) {
+            piorJogo = i + 1
+            menorPontuacao = ponto
+        }
+    }
+
+    return [quebraRecorde, piorJogo]
 }
 
-pontosPorTemporada([1])
+console.log(pontosPorTemporada(pontos));

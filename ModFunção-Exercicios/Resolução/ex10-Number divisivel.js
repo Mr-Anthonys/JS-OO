5006164856950308ac22b6d80da543dd7e23ebd3
oiDreas <andreasanthony92@gmail.com>
2024-10-28 13:46:18 -0300

    Organizando Exercícios ModFunção

--- ModFunção-Exercicios/Resolução/ex10-Number divisivel.js
+++ ModFunção-Exercicios/Resolução/ex10-Number divisivel.js
@@ -1,3 +1,8 @@
+/*
+10) ​Crie uma função que verifica se um número inteiro passado como parêmetro é divisível por 3 e retorne true 
+ou false
+*/
+
 function getRandomNumber(min, max) {
     min = Math.ceil(min);
     max = Math.ceil(max);
---
+++ ModFunção-Exercicios/Resolução/ex12-Fatorial com letras e numeros.js
@@ -0,0 +1,14 @@
+let word = "BOHR"
+let number = word.length
+
+
+function getFatorial(number) {
+    let fatorial = 1;
+
+    for (let i = number; i >= 1; i--) {
+        fatorial *= i
+    }
+    return fatorial
+}
+
+console.log(`Fatorial de ${word} (${number}): ${getFatorial(number)}`)

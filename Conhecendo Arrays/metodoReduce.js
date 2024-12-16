8cd6fd7eecda8a3d9c496c3d5e87662870832fba
oiDreas <andreasanthony92@gmail.com>
2024-12-16 16:02:27 -0300

    Add module cinco [Arrays]

--- Conhecendo Arrays/metodoReduce.js
+++ Conhecendo Arrays/metodoReduce.js
@@ -1,12 +1,20 @@
 const salaJS = [5, 6, 8, 10, 9];
-const salaJava = [5, 6, 8, 10, 9];
-const salaPhyton = [5, 6, 8, 10, 9];
+const salaJava = [5, 6, 8, 7, 9];
+const salaPhyton = [5, 6, 3, 6, 9];
 
-function getMedia(listaDeNotas) {
+function calcularMedia(listaDeNotas) {
+    //REDUCE
     const somaDasNotas = listaDeNotas.reduce((acumulador, nota) => {
         return acumulador + nota;
     }, 0);
 
+    //REDUCE REDUZIDO
+    //const somaReduzida = listaDeNotas.reduce((acumulador, nota) => acumulador + nota, 0);
+
     const media = somaDasNotas / listaDeNotas.length;
     return media;
-}
+};
+
+console.log(calcularMedia(salaJS));
+console.log(calcularMedia(salaPhyton));
+console.log(calcularMedia(salaJava));
---
+++ Conhecendo Arrays/removendoElementos.js
@@ -0,0 +1,4 @@
+const meuArray = ['Joao', 'Joao', 'Maria', 'Clara', 'Maria'];
+
+const nomesAtt = [...new Set(meuArray)];
+console.log(nomesAtt);

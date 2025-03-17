3accb782b0735cc4dcb82bd6c931feee95d858a6
oiDreas <andreasanthony92@gmail.com>
2025-03-17 19:03:04 -0300

    2

--- Mod2Avançando com ferramentas e paradigmas/Criando sua primeira biblioteca/Criando a logica do projeto/index.js
+++ Mod2Avançando com ferramentas e paradigmas/Criando sua primeira biblioteca/Criando a logica do projeto/index.js
@@ -1,11 +1,30 @@
 const fs = require('fs');
 
-const caminhoArquivo = process.argv
+//require so funciona com modulos, arquivos js
+const caminhoArquivo = process.argv; 
 const link = caminhoArquivo[2];
 
-fs.readFile(link, 'utf-8', (err, texto) => {
+
+fs.readFile(link, 'utf-8', (erro, texto) => {
+    //contadorDePalavras(texto);
+    quebrandoTexto(texto);
+});
+
+function quebrandoTexto(texto) {
+    const paragrafos = texto.toLowerCase().split('\n');
+    const contagem = paragrafos.map((paragrafo) => {
+        return contadorDePalavras(paragrafo);
+    })
     
-    //contadorDePalavras(texto.toUpperCase());
-    console.log(texto);
-    
-})
+}
+
+function contadorDePalavras(texto) {
+    const listaPalavras = texto.toLowerCase().split(' ').filter(palavras => palavras);
+    const resultado = {};
+
+    //objeto[propriedade] = valor;
+    listaPalavras.forEach((palavra) => {
+        resultado[palavra] = (resultado[palavra] || 0) + 1;
+    });
+    return console.log(resultado)
+}
